2989dd76a65f8a4491b7d50ed4b3acfcd7c04bd5
coding4freedom <chawkins2010@hotmail.com>
2024-09-04 15:34:25 -0500

    Removing Node.js, dotenv, webpack, and reverted to vanilla code

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apike
 
 */
 
-// Loading .env file 
-require('dotenv').config();
-const apiKey = process.env.ALPHA_API_KEY;
+
+const apiKey =s;
+
+async function testApi(query) {
+    const response = await fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${query}&apikey=${apiKey}`);
+
+    const data = await response.json();
+
+    const symbols = data.bestMatches || [];
+
+    const result = symbols.map(match => {
+        return `${match['1. symbol']} - ${match['2. name']}`
+    }).join('')
+
+    console.log(result);
+}
+
+testApi('app')
